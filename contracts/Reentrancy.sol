324c663c11b65dcafcef08e3c37d51574754fbe0
Kankan-0 <kankandev@protonmail.com>
2021-07-08 23:32:18 +0530

    removed usage of safemath as 8.0 solidity has breaking changes with arithmetic operations.

--- contracts/Reentrancy.sol
+++ contracts/Reentrancy.sol
@@ -1,19 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+// import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import "@openzeppelin/contracts/access/Ownable.sol";
 import 'hardhat/console.sol';
 
 contract Reentrancy is Ownable {
-    using SafeMath for uint256;
+    // using SafeMath for uint256;
     mapping (address=>uint) public balances;
     constructor() public payable {
         balances[msg.sender] = msg.value;
     }
 
     function donate(address _to) public payable {
-        balances[_to] = balances[_to].add(msg.value);
+        unchecked {
+            balances[_to] += msg.value;
+        // balances[_to] = balances[_to].add(msg.value);
+
+        }
     }
 
     function balanceOf(address _who) public view returns(uint balance){
@@ -24,7 +28,11 @@ contract Reentrancy is Ownable {
         require(balances[msg.sender] >= _amount, "Account does not have enough balance");
         (bool success, bytes memory data) = msg.sender.call{value:_amount}("");
         if(success){
-            balances[msg.sender] = balances[msg.sender].sub(_amount);
+            unchecked{
+                balances[msg.sender] -= _amount;
+                // balances[msg.sender] = balances[msg.sender].sub(_amount);
+
+            }
         }
     }
 
